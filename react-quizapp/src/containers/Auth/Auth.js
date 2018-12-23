951815645fcc3e8648b36e8d036e71ab8df467f5
Yauhen Bayeu <evvlbaev@gmail.com>
2018-12-23 22:28:23 +0300

    auth form

--- react-quizapp/src/containers/Auth/Auth.js
+++ react-quizapp/src/containers/Auth/Auth.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React from 'react'
+import classes from './Auth.module.scss'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends React.Component {
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
-        	<div>
-                <h1>AUTH</h1>
+        	<div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <Button
+                        type="success"
+                        onClick={this.loginHandler}
+                        >
+                            Login
+                        </Button>
+
+                        <Button
+                        type="primary"
+                        onClick={this.registerHandler}
+                        >
+                            Registration
+                        </Button>
+                    </form>
+                </div>
         	</div>
         );
     }
